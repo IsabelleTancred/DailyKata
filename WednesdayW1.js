7bfad19a671df10cf66a6c72cd4fa191f8833b08
IsabelleTancred <isabelleetancred@gmail.com>
2023-01-12 09:50:34 +0100

    added js file

---
+++ WednesdayW1.js
@@ -0,0 +1,24 @@
+/*The function must move all capital letters to the front of a word, lowercase letters thereafter 
+and lastly, numbers to the back. It should return a string with the reordered word.*/
+function reorder (str){
+    let upperCase = "";
+    let lowerCase = "";
+    let numbers ="";
+    
+    for (let i in str){
+      if(((str[i])*0)==0){
+        numbers=numbers+str[i]                      //check if digit
+      }
+      else if ((str[i]).toUpperCase() == str[i]){   //check if uppercase
+        upperCase=upperCase + str[i];
+      }
+      else if ((str[i]).toLowerCase()== str[i]){     //check if lowercase
+        lowerCase= lowerCase+str[i];
+      }
+    }
+   return upperCase+lowerCase+numbers;
+  }
+  
+  console.log(reorder("hA2p4Py"));
+  console.log(reorder ("m11oveMENT"));
+  console.log(reorder ("s9hOrt4CAKE"));
