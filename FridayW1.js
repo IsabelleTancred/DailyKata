7d3c34a0237d6309e585b1f639110ec9e35a197c
IsabelleTancred <isabelleetancred@gmail.com>
2023-01-13 10:07:07 +0100

    refactoring

--- FridayW1.js
+++ FridayW1.js
@@ -2,16 +2,14 @@
 number can be expressed as a sum of two or more consecutive positive numbers.*/
 
 function consecutiveSum(num) {
-        for (let i = 1; i <= (num/2); ii++) {           //the first number in a consecutive order can't be grater than half of the input value if the sum is gonna be the input value, hen num/2.
-            for (let j = i + 1; sum < num; j++) {
-                sum += j;
-                if (sum === num) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    
+  for (let i = 1; i <= (num / 2); ii++) {
+    let sum = 0;
+    for (let j = i + 1; sum < num; j++) {
+      sum += j;
+      if (sum === num) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
-    
-    
