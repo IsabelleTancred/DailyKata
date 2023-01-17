af1388f707022400c6df9f675ea0f8d935f84c32
IsabelleTancred <isabelleetancred@gmail.com>
2023-01-17 10:04:36 +0100

    minor changes

--- TuesdayW2.js
+++ TuesdayW2.js
@@ -2,19 +2,20 @@
 given letter.*/
 
 function bestFriend(string, x, y) {
-  let count = 0;
-  for (let i = 0; i < string.length - 1; ) {
-    if (string[i] === x && string[i + 1] === y) {
-      count++;
+    let count = 0;
+    for (let i = 0; i < string.length - 1; ) {
+      if (string[i] === x && string[i + 1] === y) {
+        count++;
+        i++;
+      } else if (string[i] === x) {
+        return false;
+      } else {
       i++;
-    } else if (string[i] === x) {
-      return false;
-    } else {
-    i++;
+      }
     }
+    if (string[string.length-1]===x){
+      return false;
+    }
+    return (count>0);
   }
-  if (count > 0) {
-    return true;
-  }
-  return false;
-}
+  
